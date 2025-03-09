4a582af8942b395aa2e218bdc93bc5286d9767cc
Guillaume Gauvrit <guillaume@gauvr.it>
2025-03-09 16:07:34 +0100

    Add some CI stuff

--- src/tursu/__init__.py
+++ src/tursu/__init__.py
@@ -1,5 +1,9 @@
+from importlib import metadata
+
 from .compile_all import generate_tests
 from .registry import StepRegistry, given, then, when
+
+__version__ = metadata.version("tursu")
 
 __all__ = [
     "given",
