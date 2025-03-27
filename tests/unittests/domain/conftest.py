1db7438cc9fef317e650a1410a683a37861865ff
Guillaume Gauvrit <guillaume@gauvr.it>
2025-03-27 09:08:25 +0100

    Remove useless fixtures

--- tests/unittests/domain/conftest.py
+++ tests/unittests/domain/conftest.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import pytest
 
 from tursu.domain.model.gherkin import GherkinDocument
-from tursu.runtime.runner import Tursu
 
 
 @pytest.fixture
@@ -11,10 +10,3 @@ def doc():
     return GherkinDocument.from_file(
         Path(__file__).parent / "fixtures" / "login.feature"
     )
-
-
-@pytest.fixture
-def registry() -> Tursu:
-    import tests.unittests.domain.fixtures
-
-    return Tursu().scan(tests.unittests.domain.fixtures)
--- tests/unittests/runtime/conftest.py
+++ tests/unittests/runtime/conftest.py
@@ -1,24 +1,6 @@
-from pathlib import Path
-
 import pytest
 
-from tursu.domain.model.gherkin import GherkinDocument
 from tursu.runtime.registry import Tursu
-
-
-@pytest.fixture
-def docs_dir():
-    return Path(__file__).parent / "fixtures"
-
-
-@pytest.fixture
-def doc(docs_dir: Path):
-    return GherkinDocument.from_file(docs_dir / "scenario.feature")
-
-
-@pytest.fixture
-def outline_doc(docs_dir: Path):
-    return GherkinDocument.from_file(docs_dir / "scenario_outline.feature")
 
 
 @pytest.fixture(scope="session")
